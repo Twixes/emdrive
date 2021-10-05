48a992e9490e2f7eeaf03463984a269f568e2201
Michael Matloka <dev@twixes.com>
2021-10-05 13:26:44 +0200

    Remove useless metrics code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,5 @@
 pub mod config;
 pub mod data;
-mod metrics;
 mod queries;
 pub mod tcp;
 mod utils;
-
-impl queries::Distancable<metrics::hamming::Distance> for metrics::hamming::Position {
-    fn distance(&self, other: &Self) -> metrics::hamming::Distance {
-        metrics::hamming::distance(self, other)
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use emdrive::{config, tcp};
+use emdrive::{config, tcp, timeprintln};
 
 #[tokio::main]
 async fn main() {
+    timeprintln!("🔢 Starting Emdrive…");
     let config = config::Config::new();
+    timeprintln!("⚙️ Configuration:\n{}", config);
     tcp::start_server(config).await;
 }
