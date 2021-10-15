6a079599c59d360ab8a2151aa8bef82da08e1286
Michael Matloka <dev@twixes.com>
2021-10-16 00:52:39 +0200

    Add `human-panic`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use emdrive::{Config, Instance};
+use emdrive::Instance;
+use human_panic::setup_panic;
 use log::*;
 
 #[tokio::main]
 async fn main() {
+    setup_panic!(Metadata {
+        name: "Emdrive".into(),
+        version: env!("CARGO_PKG_VERSION").into(),
+        authors: "".into(),
+        homepage: "https://github.com/Twixes/emdrive".into(),
+    });
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("emdrive=info"))
         .init();
     info!("🔢 Starting Emdrive...");
